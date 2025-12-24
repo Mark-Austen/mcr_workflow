e5f35ff963b60832cc1a9d1b1eb7ae9c311c7c70
Mark Austen <117334224+Mark-Austen@users.noreply.github.com>
2025-12-24 13:08:27 +1100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "mp1mabucket"
+    key    = "path/to/my/key"
+    region = "ap-southeast-2"
+  }
+  required_providers {
+    megaport = {
+      source  = "megaport/megaport"
+      version = "1.4.6"
+    }
+  }
+}
 
+provider "megaport" {
+  environment           = "staging"
+  accept_purchase_terms = true
+}
