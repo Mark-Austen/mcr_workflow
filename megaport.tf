063c93965cc35c1cba26619ffd61b142a82aa639
Mark Austen <117334224+Mark-Austen@users.noreply.github.com>
2025-12-29 09:07:40 +1000

    Update megaport.tf

--- megaport.tf
+++ megaport.tf
@@ -4,3 +4,11 @@ resource "megaport_mcr" "mcr" {
   location_id          = 2
   contract_term_months = 1
 }
+
+resource "megaport_mcr" "mcr2" {
+  product_name         = "MCR2"
+  port_speed           = 1000
+  location_id          = 2
+  contract_term_months = 1
+}
+
